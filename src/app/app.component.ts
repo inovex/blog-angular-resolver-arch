4791b979ade76153b07a23866c9b31ed2d4a92f3
Kirill Rakhman <kirill.rakhman@inovex.de>
2022-04-11 12:01:57 +0200

    show currently opened details in navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RoutingService } from './routing.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+
+  constructor(private routingService: RoutingService) {
+  }
+
+  currentDetails$ = this.routingService.detailsData$;
 }
---
+++ src/app/routing.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { filter, Observable, of, switchMap } from 'rxjs';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+
+function findDetailsRoute(ars: ActivatedRoute): ActivatedRoute | undefined {
+  if (ars.outlet === 'details') {
+    return ars;
+  }
+
+  for (const child of ars.children) {
+    const childResult = findDetailsRoute(child);
+
+    if (childResult) {
+      return childResult;
+    }
+  }
+
+  return undefined;
+}
+
+@Injectable({ providedIn: 'root' })
+export class RoutingService {
+  constructor(private router: Router) {
+  }
+
+  detailsData$: Observable<any | undefined> = this.router.events.pipe(
+    filter(x => x instanceof NavigationEnd),
+    switchMap(() => {
+      const activatedRoute = this.router.routerState.root;
+      const detailsDataObservable = findDetailsRoute(activatedRoute)?.data;
+      return detailsDataObservable ?? of(undefined);
+    }),
+  );
+}
